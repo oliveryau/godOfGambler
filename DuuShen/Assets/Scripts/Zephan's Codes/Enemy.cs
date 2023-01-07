182f120a3f6732b1ece631f7bb7a83310af257a0
zephan2000 <zephanwong@hotmail.com>
2023-01-07 15:04:10 +0800

    Unnecessary codes taken out of script

--- DuuShen/Assets/Scripts/Zephan's Codes/Enemy.cs
+++ DuuShen/Assets/Scripts/Zephan's Codes/Enemy.cs
@@ -19,7 +19,7 @@ public class Enemy : MonoBehaviour
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
-        SetEnemyValues();
+        //SetEnemyValues();
     }
 
     void Update()
@@ -27,12 +27,12 @@ public class Enemy : MonoBehaviour
         Swarm();   
     }
 
-    private void SetEnemyValues()
-    {
-        GetComponent<Health>().SetHealth(data.hp, data.hp);
-        damage = data.damage;
-        speed = data.speed;
-    }
+    //private void SetEnemyValues()
+    //{
+    //    GetComponent<Health>().SetHealth(data.hp, data.hp);
+    //    damage = data.damage;
+    //    speed = data.speed;
+    //}
     private void Swarm()
     {
         transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
@@ -40,11 +40,11 @@ public class Enemy : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        if (collider.CompareTag("Player"))
-        {
-           collider.GetComponent<Health>().Damage(damage);
-           this.GetComponent<Health>().Damage(10000);
-        }
+        //if (collider.CompareTag("Player"))
+        //{
+        //   collider.GetComponent<Health>().Damage(damage);
+        //   this.GetComponent<Health>().Damage(10000);
+        //}
 
         if (collider.gameObject.CompareTag("Enemy")) //Check Slows tag
         {
