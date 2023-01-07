61a2b16029d41de6158183d68bcc75ff2b014334
zephan2000 <zephanwong@hotmail.com>
2023-01-07 11:57:54 +0800

    Enemy stuff

---
+++ DuuShen/Assets/Scripts/Enemy.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private int damage = 5;
+    [SerializeField]
+    private float speed = 1.5f;
+    [SerializeField]
+    private EnemyData data;
+
+    private GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        SetEnemyValues();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void SetEnemyValues()
+    {
+        GetComponent<Health>().SetHealth(data.hp, data.hp);
+        damage = data.damage;
+        speed = data.speed;
+    }
+    private void Swarm()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+           collider.GetComponent<Health>().Damage(damage);
+           this.GetComponent<Health>().Damage(10000);
+        }
+    }
+}
