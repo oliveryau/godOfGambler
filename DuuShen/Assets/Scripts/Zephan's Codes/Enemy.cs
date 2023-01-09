25f541dde54ee2780180f564f7d8ded35cbb318e
oliveryau <55869156+oliveryau@users.noreply.github.com>
2023-01-09 13:52:37 +0800

    zephan

--- DuuShen/Assets/Scripts/Zephan's Codes/Enemy.cs
+++ DuuShen/Assets/Scripts/Zephan's Codes/Enemy.cs
@@ -5,52 +5,129 @@ using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField]
-    private int damage = 5;
+    //[SerializeField]
+    //private int damage = 5;
     [SerializeField]
     private float speed = 1.5f;
     [SerializeField]
     private EnemyData data;
-    [SerializeField] 
+    [SerializeField]
     private Text statusText;
+    [SerializeField]
+    private float waitTime = 0.3f;
 
     private GameObject player;
+    public Transform pathHolder;
+    Transform playerlocation;
+
+    public Light spotlight;
+    public float viewDistance;
+    public LayerMask viewMask;
+    float viewAngle;
+
 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
-        SetEnemyValues();
+        playerlocation = GameObject.FindGameObjectWithTag("Player").transform;
+        //SetEnemyValues();
+        //Vector2[] waypoints = new Vector2[pathHolder.childCount];
+        //for(int i = 0; i < waypoints.Length; i++)
+        //{
+        //    waypoints[i] = pathHolder.GetChild(i).position; 
+        //}
+
+        //StartCoroutine(FollowPath(waypoints));
     }
 
     void Update()
     {
-        Swarm();   
+        if (CanSeePlayer())
+        {
+            Swarm();
+        }
+
     }
 
-    private void SetEnemyValues()
+    //IEnumerator FollowPath (Vector2[] waypoints)
+    //{
+    //    transform.position = waypoints [0];
+    //    int targetWaypointIndex = 1;
+    //    Vector2 targetWaypoint = waypoints[targetWaypointIndex];
+    //    while(true)
+    //    {
+    //        transform.position = Vector2.MoveTowards(transform.position, targetWaypoint, speed * Time.deltaTime);
+    //        if((Vector2)transform.position == targetWaypoint)
+    //        {
+    //            targetWaypointIndex = (targetWaypointIndex + 1) % waypoints.Length;
+    //            targetWaypoint = waypoints [targetWaypointIndex];
+    //            yield return new WaitForSeconds(waitTime);
+
+    //        }
+    //        yield return null;
+    //    }
+    //}
+
+
+
+    bool CanSeePlayer()
     {
-        GetComponent<Health>().SetHealth(data.hp, data.hp);
-        damage = data.damage;
-        speed = data.speed;
+        if (Vector2.Distance(transform.position, playerlocation.position) < viewDistance) // within distance
+        {
+            Vector2 dirToPlayer = (playerlocation.position - transform.position).normalized;
+            float angleBetweenGuardAndPlayer = Vector2.Angle(transform.forward, dirToPlayer);
+            if (angleBetweenGuardAndPlayer < viewAngle / 2f) //within viewAngle 
+            {
+                if (!Physics.Linecast(transform.position, playerlocation.position, viewMask))
+                {
+                    return true; // all 3 conditions met (within angle, within distance and no object blocking)
+                }
+            }
+        }
+        return false;
     }
+
+    //private void SetEnemyValues()
+    //{
+    //    GetComponent<Health>().SetHealth(data.hp, data.hp);
+    //    damage = data.damage;
+    //    speed = data.speed;
+    //}
+
+    //void OnDrawGizmos()
+    //{
+    //    Vector2 startPosition = pathHolder.GetChild(0).position;
+    //    Vector2 previousPosition = startPosition;
+    //    foreach (Transform waypoint in pathHolder)
+    //    {
+    //        Gizmos.DrawSphere(waypoint.position, 0.3f);
+    //        Gizmos.DrawLine(previousPosition, waypoint.position);
+    //        previousPosition = waypoint.position;
+    //    }
+    //    Gizmos.DrawLine(previousPosition, startPosition);
+    //    Gizmos.color = Color.red;
+    //    Gizmos.DrawRay(transform.position, transform.forward * viewDistance);
+    //}
     private void Swarm()
     {
         transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
     }
 
-    private void OnTriggerEnter2D(Collider2D collider)
-    {
-        if (collider.CompareTag("Player"))
-        {
-           collider.GetComponent<Health>().Damage(damage);
-           this.GetComponent<Health>().Damage(10000);
-        }
+    //private void OnTriggerEnter2D(Collider2D collider)
+    //{
+    //if (collider.CompareTag("Player"))
+    //{
+    //   collider.GetComponent<Health>().Damage(damage);
+    //   this.GetComponent<Health>().Damage(10000);
+    //}
 
-        if (collider.gameObject.CompareTag("Enemy")) //Check Slows tag
-        {
-            Destroy(collider.gameObject);
-            statusText.text = "Enemy Defeated!";
+    //if (collider.gameObject.CompareTag("Enemy")) //Check Slows tag
+    //{
+    //    Destroy(collider.gameObject);
+    //    statusText.text = "Enemy Defeated!";
 
-        }
-    }
+    //}
+    //}
+
+
 }
---
+++ DuuShen/Assets/Scripts/Zephan's Codes/EnemyMovement.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1f;
+    Rigidbody2D myRigidbody;
+    // Start is called before the first frame update
+
+    [SerializeField] float enemySpeed = 1f;       //HERE are the presets for your enemy! feel free to change any of the values here
+    [SerializeField] float startFollowRange = 5f; //only in the inspector though because the inspectors values override these values
+    [SerializeField] float damageRange = 0.5f;
+    [SerializeField] float damageAgainDelay = 1f;
+
+    float distance = 0f; //These check don't need to be changed anywhere cause its set by code
+    bool damagePlayerCalled = false;
+    bool damagedPlayer = false;
+
+    Collider2D colliderType;
+    GameObject player;
+    Coroutine playerTracker;
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        colliderType = GetComponent<Collider2D>();
+
+        player = GameObject.FindGameObjectWithTag("Player");  //If you want to find by Tag this is what you do
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsFacingRight() == true)
+        {
+            myRigidbody.velocity = new Vector2(moveSpeed, 0f);
+        }
+        else
+        {
+            myRigidbody.velocity = new Vector2(-moveSpeed, 0f);
+        }
+
+
+        CheckForPlayer(); //We check if player is in range every frame
+
+
+    }
+
+    private void CheckForPlayer()
+    {
+        distance = Vector2.Distance(transform.position, player.transform.position); //calculate the distance
+
+        if (distance < startFollowRange && !colliderType.IsTouchingLayers(LayerMask.GetMask("Ground"))) //Check if player is in following range
+        {
+            MoveToPlayer();
+        }
+    }
+    private IEnumerator DamagePlayer()
+    {
+        damagePlayerCalled = true;
+
+        if (damagedPlayer)
+        {
+            yield return new WaitForSeconds(damageAgainDelay); damagedPlayer = false;
+        } //If first time getting in range damage immediately
+
+        Debug.Log("Damage player!"); //You can put whatever you want here
+        damagedPlayer = true;
+        damagePlayerCalled = false;
+    }
+
+    private void MoveToPlayer()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, enemySpeed * Time.deltaTime); //simply moves from our position to player position
+
+        if (distance < damageRange && !damagePlayerCalled) //We say "&&" to say "and" in an if statement, and i check for if damagePlayerCalled == true
+        {                                                  //so we don't call the coroutine multiple times
+            StartCoroutine(DamagePlayer());
+        }
+    }
+
+    private bool IsFacingRight()
+    {
+        return transform.localScale.x > Mathf.Epsilon; //checking for an extremely small x value if x-value is > 0.0001 it is right < 0.001 is left
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        //Turn
+        transform.localScale = new Vector2(-(Mathf.Sign(myRigidbody.velocity.x)), transform.localScale.y);
+
+    }
+
+    //void OnTriggerEnter2D(Collider2D colliderType)
+    //{
+    //    if (colliderType.IsTouchingLayers(LayerMask.GetMask("Ground")))
+    //    {
+    //        StopCoroutine(MoveToPlayer());
+    //    }
+    //}
+}
