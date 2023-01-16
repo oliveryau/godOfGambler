f64504ed081a490a8b86203462cc77553ac836ce
oliveryau <55869156+oliveryau@users.noreply.github.com>
2023-01-16 18:26:25 +0800

    falling platforms

---
+++ DuuShen/Assets/Scripts/PlatformManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformManager : MonoBehaviour
+{
+    public static PlatformManager Instance = null;
+    [SerializeField] private GameObject platformPrefab;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(platformPrefab, new Vector2(49f, -1.5f), platformPrefab.transform.rotation);
+    }
+
+    public IEnumerator SpawnPlatform(Vector2 spawnPosition)
+    {
+        yield return new WaitForSeconds(2f);
+        Instantiate(platformPrefab, spawnPosition, platformPrefab.transform.rotation);
+    }
+}
