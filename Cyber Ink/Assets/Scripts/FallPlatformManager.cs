1ca04832077df4913af322daa3a85e0588a5d908
Oliver Yau <55869156+oliveryau@users.noreply.github.com>
2023-02-21 02:18:06 +0800

    stuff

--- Cyber Ink/Assets/Scripts/FallPlatformManager.cs
+++ Cyber Ink/Assets/Scripts/FallPlatformManager.cs
@@ -22,11 +22,20 @@ public class FallPlatformManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(platformPrefab, new Vector2(166f, 13f), platformPrefab.transform.rotation);
-        Instantiate(platformPrefab, new Vector2(106.5f, 4f), platformPrefab.transform.rotation);
-        Instantiate(platformPrefab, new Vector2(179f, -48.5f), platformPrefab.transform.rotation);
+        //Top side
+        Instantiate(platformPrefab, new Vector2(170f, 15f), platformPrefab.transform.rotation);
+        Instantiate(platformPrefab, new Vector2(165f, 15f), platformPrefab.transform.rotation);
+        Instantiate(platformPrefab, new Vector2(106.5f, 4.5f), platformPrefab.transform.rotation);
+        Instantiate(platformPrefab, new Vector2(128f, -2.25f), platformPrefab.transform.rotation);
+
+        //Right bottom side
+        Instantiate(platformPrefab, new Vector2(177f, -48.25f), platformPrefab.transform.rotation);
+        Instantiate(platformPrefab, new Vector2(126f, -52.75f), platformPrefab.transform.rotation);
+        Instantiate(platformPrefab, new Vector2(120f, -58.75f), platformPrefab.transform.rotation);
         Instantiate(platformPrefab, new Vector2(167.75f, -46.5f), platformPrefab.transform.rotation);
         Instantiate(platformPrefab, new Vector2(163.5f, -46.5f), platformPrefab.transform.rotation);
+
+        //Left bottom side
         Instantiate(platformPrefab, new Vector2(86f, -67.5f), platformPrefab.transform.rotation);
     }
 
